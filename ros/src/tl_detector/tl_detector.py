f6fb34a7b56df988fbfad54d744441dd23469f61
Fernando <ferorga1@gmail.com>
2019-05-20 21:49:03 +0200

    Corrected error in tab and whitespaces in the tl_detector

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -149,24 +149,21 @@ class TLDetector(object):
         if self.pose:
             car_wp_idx = self.get_closest_waypoint(self.pose.pose.position.x, self.pose.pose.position.y)
 
-        # TODO find the closest visible traffic light (if one exists)
-
-        diff = 0
-        if self.waypoints.waypoints is not None:
+            # TODO find the closest visible traffic light (if one exists)
             diff = len(self.waypoints.waypoints)
 
-        for i, light in enumerate(self.lights):
-            line = stop_line_positions[i]
-            temp_wp_idx = self.get_closest_waypoint(line[0], line[1])
-            d = temp_wp_idx - car_wp_idx
-            if 0 <= d < diff:
-                diff = d
-                closest_light = light
-                line_wp_idx = temp_wp_idx
+            for i, light in enumerate(self.lights):
+                line = stop_line_positions[i]
+                temp_wp_idx = self.get_closest_waypoint(line[0], line[1])
+                d = temp_wp_idx - car_wp_idx
+                if 0 <= d < diff:
+                    diff = d
+                    closest_light = light
+                    line_wp_idx = temp_wp_idx
 
-            if closest_light:
-                state = self.get_light_state(closest_light)
-                return line_wp_idx, state
+        if closest_light:
+            state = self.get_light_state(closest_light)
+            return line_wp_idx, state
 
         return -1, TrafficLight.UNKNOWN
 
